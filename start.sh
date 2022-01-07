b674ee3eaaf9e5cc3c165e13e61dba7cdb2db2d2
shahin137 <96627667+shahin137@users.noreply.github.com>
2022-01-07 13:25:13 +0330

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
-python bot.py +[phone1] doge
-python bot.py +[phone1] zec
-python bot.py +[phone1] ltc
-python bot.py +[phone1] btc
-python bot.py +[phone1] bch
+python bot.py +989185766975 doge
+python bot.py +989185766975 zec
+python bot.py +989185766975 ltc
+python bot.py +989185766975 btc
+python bot.py +989185766975 bch
 clear
 pwd
 bash start.sh
 
 #Replace [phone] with your Number Phone
 #ext 
-   python bot.py +62xxxx doge
+   python bot.py +989185766975
